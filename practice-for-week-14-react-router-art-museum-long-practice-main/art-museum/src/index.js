fcdb152ea82b727bf4fef76e77605849c1acd531
Naim Miah <mmiah@fordham.edu>
2023-08-31 16:37:40 -0400

    changes

--- practice-for-week-14-react-router-art-museum-long-practice-main/art-museum/src/index.js
+++ practice-for-week-14-react-router-art-museum-long-practice-main/art-museum/src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Root from "./Root";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
